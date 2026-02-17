5471d3cd58f48b1ec18d82246a97e7470020ca42
Sebastian <ss.bodea@gmail.com>
2026-02-17 02:59:35 +0200

    Initial commit

--- app/src/main/java/com/ssbodea/workout_tracker/data/database/ExerciseDatabase.kt
+++ app/src/main/java/com/ssbodea/workout_tracker/data/database/ExerciseDatabase.kt
@@ -1,60 +1,90 @@
 package com.ssbodea.workout_tracker.data.database
 
 object ExerciseDatabase {
-    val muscleGroups = listOf("Legs", "Core", "Push", "Pull")
+
+    val muscleGroups = listOf(
+        "Legs",
+        "Push",
+        "Pull",
+        "Core Dynamic",
+        "Neck",
+        "Core Isometrics",
+        "Push Isometrics",
+        "Pull Isometrics",
+        "Grip Isometrics",
+        "Flexibility"
+    )
 
     private val exercisesMap = mapOf(
         "Legs" to listOf(
             "Squat",
-            "Leg Press",
-            "Side Split",
-            "Front Split",
+            "Lunge",
             "Pistol Squat",
             "Dragon Squat"
         ),
-        "Core" to listOf(
-            "Plank",
-            "Hollow Hold",
-            "Arch Hold",
-            "Side Plank",
-            "Reverse Plank",
-            "Sit-ups",
-            "Reverse Crunch",
-            "Ab Wheel Rollout",
-            "Hanging Leg Raise",
-            "L-sit",
-            "V-sit",
-            "Toes to Bar",
-            "Windshield Wipers",
-            "Dragon Flag",
-            "Dragon Press"
-        ),
         "Push" to listOf(
-            "Bench Press",
-            "Shoulder Press",
             "Push-up",
-            "Dips",
+            "Diamond Push-up",
+            "Archer Push-up",
+            "One-Arm Push-up",
             "Pike Push-up",
             "Handstand Push-up",
-            "One Arm Push-up",
-            "Planche Push-up",
-            "One Arm Handstand Hold",
-            "One Arm Planche"
+            "Dips",
+            "Planche Push-up"
         ),
         "Pull" to listOf(
-            "Deadlift",
-            "Bent Over Row",
-            "Seated Row",
-            "Back Extension",
-            "Curl",
-            "Pull-up",
+            "Inverted Row",
             "Chin-up",
-            "Muscle-up",
-            "Back Lever",
+            "Pull-up",
+            "Archer Pull-up",
+            "Muscle-Up",
+            "One-Arm Row",
+            "One-Arm Pull-up",
+            "One-Arm Muscle-up"
+        ),
+        "Core Dynamic" to listOf(
+            "Sit-up",
+            "Reverse Crunch",
+            "Hanging Knee Raise",
+            "Hanging Leg Raise",
+            "Toes-to-Bar",
+            "Windshield Wipers",
+            "Dragon Flag"
+        ),
+        "Neck" to listOf(
+            "Neck Flexion",
+            "Neck Extension",
+            "Neck Side Flexion",
+            "Neck Bridge"
+        ),
+        "Core Isometrics" to listOf(
+            "Plank",
+            "Side Plank",
+            "Hollow Hold",
+            "Reverse Plank",
+            "L-sit",
+            "V-sit",
+            "Dragon Press"
+        ),
+        "Push Isometrics" to listOf(
+            "Arch Hold",
+            "Handstand Hold",
+            "Planche Hold"
+        ),
+        "Pull Isometrics" to listOf(
             "Front Lever",
-            "Human Flag",
-            "One Arm Pull-up",
-            "One Arm Muscle-up"
+            "Back Lever",
+            "Human Flag"
+        ),
+        "Grip Isometrics" to listOf(
+            "Dead Hang",
+            "Towel Hang",
+            "One-Arm Hang",
+            "Hand-gripper"
+        ),
+        "Flexibility" to listOf(
+            "Front Split",
+            "Side Split"
         )
     )
 
